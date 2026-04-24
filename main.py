85172b733471ce94007dcdcde2c0e4017af30f4a
Morbanaa <theodorerodd@gmail.com>
2026-04-24 17:16:25 -0500

    program now reads from file and stores data in var

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # World Series Winners
 
 def main():
-    pass
+    infile = open("WorldSeriesWinners.txt", "r")
+    file_contents = infile.read()
+    print(file_contents)
 
 if __name__ == "__main__":
     main()
